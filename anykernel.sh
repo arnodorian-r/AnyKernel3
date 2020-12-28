01ff0ddbf7b3bbb3504d630be15ba37b7762e436
arnodorian-r <geekroy98@gmail.com>
2020-12-28 10:29:14 +0530

    Simplify

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel
+kernel.string=Polar-Kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -26,16 +26,8 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
-
 ## AnyKernel install
 dump_boot;
 
 write_boot;
 ## end install
-
